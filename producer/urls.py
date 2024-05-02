0c86e395e4f4364e4fe4235e9fc572d2356f765b
iden7 <veabuluklobre@gmail.com>
2024-05-02 18:39:22 +0600

    add detail

--- producer/urls.py
+++ producer/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import create_project
+from .views import create_project, project_list, delete_project, project_detail
 
 urlpatterns = [
     path('create/', create_project, name='create_project'),
+    path('list/', project_list, name='project_list'),
+    path('<int:pk>/delete/', delete_project, name='delete_project'),
+    path('<int:pk>/', project_detail, name='project_detail'),
 ]
--- producer/views.py
+++ producer/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProjectForm
+from .models import Project
 
 def create_project(request):
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('project_list')
     else:
         form = ProjectForm()
     return render(request, 'create_project.html', {'form': form})
+
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'project_list.html', {'projects': projects})
+
+def delete_project(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    project.delete()
+    return redirect('project_list')
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'project_detail.html', {'project': project})
