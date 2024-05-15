78cd11555d8c50c3694ed3467245262c685baf7c
iden7 <veabuluklobre@gmail.com>
2024-05-15 18:06:22 +0600

    user app

--- studio/urls.py
+++ studio/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('producer.urls'))
+    path('', include('producer.urls')),
+    path('users/', include('users.urls'))
 ]
